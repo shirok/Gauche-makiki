8180e5788fd35fb9bb041f74f2c951f5304f77b5
Shiro Kawai <shiro@acm.org>
2018-02-10 23:19:58 -1000

    Use OS-chosen port instead of fixed port for testing

--- test.scm
+++ test.scm
@@ -16,89 +16,89 @@
 (use makiki)
 (test-module 'makiki)
 
-(define *port* 8359)
-(define *server* #`"localhost:,*port*")
-
 (define (call-with-server path proc)
-  (let ((p (run-process `(gosh "-I." ,path "--port" ,*port*)
-                        :output :pipe :error :pipe :wait #f)))
-    (let1 msg (read-line (process-error p))
-      (unless (#/started/i msg)
-        (errorf "failed to start server script ~s: ~a" path msg)))
-    (unwind-protect (proc p)
+  (let* ([p (run-process `(gosh "-I." ,path "--port" 0)
+                            :output :pipe :error :pipe :wait #f)]
+         [port (get-port p)]
+         [host #"localhost:~port"])
+    (unwind-protect (proc p host port)
       (process-kill p))))
 
 (define (call-with-server/exit-status path proc)
-  (let ((p (run-process `(gosh "-I." ,path "--port" ,*port*)
-                        :output :pipe :error :pipe :wait #f)))
-    (let1 msg (read-line (process-error p))
-      (unless (#/started/i msg)
-        (errorf "failed to start server script ~s: ~a" path msg)))
-    (proc p)
+  (let* ([p (run-process `(gosh "-I." ,path "--port" 0)
+                         :output :pipe :error :pipe :wait #f)]
+         [port (get-port p)]
+         [host #"localhost:~port"])
+    (proc p host port)
     (process-wait p)
     (sys-wait-exit-status (process-exit-status p))))
+
+(define (get-port proc)
+  (rxmatch-case (read-line (process-error proc))
+    [#/started on \(.*:(\d+)\)/i (_ pp) (x->integer pp)]
+    [else => (cut errorf "failed to start server script ~s: ~a" path <>)]))
 
 ;;;
 (test-section "basic functionality")
 
 ($ call-with-server "tests/basic.scm"
-   (^p
-    (test* "basic responds 404" "404"
-           (values-ref (http-get *server* "/") 0))
+   (^[p s t]
+     (test* "basic responds 404" "404"
+            (values-ref (http-get s "/") 0))
 
-    (test* "bad request format" (eof-object)
-           (let1 s (make-client-socket 'inet "localhost" *port*)
-             (socket-shutdown s SHUT_WR)
-             (unwind-protect (read-line (socket-input-port s))
-               (socket-close s))))
+     (test* "bad request format" (eof-object)
+            (let1 s (make-client-socket 'inet "localhost" t)
+              (socket-shutdown s SHUT_WR)
+              (unwind-protect (read-line (socket-input-port s))
+                (socket-close s))))
 
-    (test* "bad request format" "HTTP/1.1 501 Not Implemented"
-           (let1 s (make-client-socket 'inet "localhost" *port*)
-             (unwind-protect
-                 (begin
-                   (display "ZZZZ /foo.txt HTTP/1.1\r\n" (socket-output-port s))
-                   (socket-shutdown s SHUT_WR)
-                   (read-line (socket-input-port s)))
-               (socket-close s))))
-    ))
+     (test* "bad request format" "HTTP/1.1 501 Not Implemented"
+            (let1 s (make-client-socket 'inet "localhost" t)
+              (unwind-protect
+                  (begin
+                    (display "ZZZZ /foo.txt HTTP/1.1\r\n" (socket-output-port s))
+                    (socket-shutdown s SHUT_WR)
+                    (read-line (socket-input-port s)))
+                (socket-close s))))
+     ))
 
 ;;;
 (test-section "request methods")
 
 ($ call-with-server "tests/methods.scm"
-   (^p
-    (test* "GET"    "get"    (values-ref (http-get *server* "/") 2))
-    (test* "POST"   "post"   (values-ref (http-post *server* "/" "") 2))
-    (test* "PUT"    "put"    (values-ref (http-put *server* "/" "") 2))
-    (test* "DELETE" "delete" (values-ref (http-delete *server* "/") 2))
-    
-    (test* "OPTIONS" "HTTP/1.1 404 Not Found"
-           (let1 s (make-client-socket 'inet "localhost" *port*)
-             (unwind-protect
-                 (begin
-                   (display "OPTIONS / HTTP/1.1\r\n" (socket-output-port s))
-                   (socket-shutdown s SHUT_WR)
-                   (read-line (socket-input-port s)))
-               (socket-close s))))
-    ))
+   (^[p s t]
+     (test* "GET"    "get"    (values-ref (http-get s "/") 2))
+     (test* "POST"   "post"   (values-ref (http-post s "/" "") 2))
+     (test* "PUT"    "put"    (values-ref (http-put s "/" "") 2))
+     (test* "DELETE" "delete" (values-ref (http-delete s "/") 2))
+     
+     (test* "OPTIONS" "HTTP/1.1 404 Not Found"
+            (let1 s (make-client-socket 'inet "localhost" t)
+              (unwind-protect
+                  (begin
+                    (display "OPTIONS / HTTP/1.1\r\n" (socket-output-port s))
+                    (socket-shutdown s SHUT_WR)
+                    (read-line (socket-input-port s)))
+                (socket-close s))))
+     ))
 
 ;;;
 (test-section "file-handler")
 
 ($ call-with-server "tests/file.scm"
-   (^p
-    (define (file-test path ctype)
-      (receive (code hdrs body) (http-get *server* #`"/,path")
-        (test* (format "file test ~s" path) `("200" ,ctype #t)
-               (list code (rfc822-header-ref hdrs "content-type")
-                     (equal? (file->string path) body)))))
-    (file-test "tests/file.scm" "text/plain")
-    (file-test "tests/file-dummy.js" "application/javascript; charset=utf-8")
-    (file-test "tests/file-dummy.gif" "image/gif")
-    (file-test "tests/file-dummy.jpg" "image/jpeg")
-    (file-test "tests/file-dummy.png" "image/png")
-    (file-test "tests/file-dummy.css" "text/css")
-    ))
+   (^[p s t]
+     (define (file-test path ctype)
+       (receive (code hdrs body) (http-get s #`"/,path")
+         (test* (format "file test ~s" path) `("200" ,ctype #t)
+                (list code (rfc822-header-ref hdrs "content-type")
+                      (equal? (file->string path) body)))))
+     (file-test "tests/file.scm" "text/plain")
+     (file-test "tests/file-dummy.js" "application/javascript; charset=utf-8")
+     (file-test "tests/file-dummy.gif" "image/gif")
+     (file-test "tests/file-dummy.jpg" "image/jpeg")
+     (file-test "tests/file-dummy.png" "image/png")
+     (file-test "tests/file-dummy.css" "text/css")
+     ))
 
 ;;;
 (test-section "cgi-handler and cgi-script")
@@ -107,22 +107,22 @@
 
 (define (test-cgi-stuff server-file)
   ($ call-with-server server-file
-     (^p
-      (test* #`"(,server-file) empty parameters" (get-environment-variables)
-             (assq-ref (read-from-string (values-ref (http-get *server* "/") 2))
-                       'environments)
-             (cut lset= equal? <> <>))          
-      (test* #`"(,server-file )with parameters"
-             '(("a" ")(!#%%$!*^({}<>" "wat?") ("b" ""))
-             (assq-ref (read-from-string
-                        (values-ref (http-get *server* '("/"
-                                                         (a ")(!#%%$!*^({}<>")
-                                                         (b "")
-                                                         (a "wat?")))
-                                    2))
-                       'parameters)
-             (cut lset= equal? <> <>))
-      )))
+     (^[p s t]
+       (test* #`"(,server-file) empty parameters" (get-environment-variables)
+              (assq-ref (read-from-string (values-ref (http-get s "/") 2))
+                        'environments)
+              (cut lset= equal? <> <>))          
+       (test* #`"(,server-file )with parameters"
+              '(("a" ")(!#%%$!*^({}<>" "wat?") ("b" ""))
+              (assq-ref (read-from-string
+                         (values-ref (http-get s '("/"
+                                                   (a ")(!#%%$!*^({}<>")
+                                                   (b "")
+                                                   (a "wat?")))
+                                     2))
+                        'parameters)
+              (cut lset= equal? <> <>))
+       )))
 
 (test-cgi-stuff "tests/test-env1.scm")
 (test-cgi-stuff "tests/test-env2.scm")
@@ -131,54 +131,54 @@
 (test-section "customized respond/ng body")
 
 ($ call-with-server "tests/customized-ng.scm"
-   (^p
-    (test* "custom 404 response body"
-           '("404" "text/html; charset=utf-8"
-             "<html><title>404 Not found</title><body><p>I don't have that!</p></body></html>")
-           (receive (code hdrs body) (http-get *server* "/")
-             (list code (rfc822-header-ref hdrs "content-type") body)))))
+   (^[p s t]
+     (test* "custom 404 response body"
+            '("404" "text/html; charset=utf-8"
+              "<html><title>404 Not found</title><body><p>I don't have that!</p></body></html>")
+            (receive (code hdrs body) (http-get s "/")
+              (list code (rfc822-header-ref hdrs "content-type") body)))))
 
 ($ call-with-server "tests/customized-ng.scm"
-   (^p
-    (test* "error in custom 404 response body"
-           '("404" "text/plain; charset=utf-8"
-             "Not Found")
-           (receive (code hdrs body) (http-get *server* "/favicon.ico")
-             (list code (rfc822-header-ref hdrs "content-type") body)))))
+   (^[p s t]
+     (test* "error in custom 404 response body"
+            '("404" "text/plain; charset=utf-8"
+              "Not Found")
+            (receive (code hdrs body) (http-get s "/favicon.ico")
+              (list code (rfc822-header-ref hdrs "content-type") body)))))
 
 ($ call-with-server "tests/customized-ng.scm"
-   (^p
-    (test* "request-error condition"
-           '("400" "application/json; charset=utf-8"
-             "{\"message\":\"boo!\"}")
-           (receive (code hdrs body) (http-get *server* "/request-error")
-             (list code (rfc822-header-ref hdrs "content-type") body)))))
+   (^[p s t]
+     (test* "request-error condition"
+            '("400" "application/json; charset=utf-8"
+              "{\"message\":\"boo!\"}")
+            (receive (code hdrs body) (http-get s "/request-error")
+              (list code (rfc822-header-ref hdrs "content-type") body)))))
 
 ;;;
 (test-section "sxml template")
 
 ($ call-with-server "tests/sxml-tmpl.scm"
-   (^p
-    (test* "sxml-tmpl"
-           "<html><body><p>Yo, Keoki.  Howzit?</p></body></html>"
-           (values-ref (http-get *server* "/?g=2&name=Keoki") 2))))
+   (^[p s t]
+     (test* "sxml-tmpl"
+            "<html><body><p>Yo, Keoki.  Howzit?</p></body></html>"
+            (values-ref (http-get s "/?g=2&name=Keoki") 2))))
 
 ;;;
 (test-section "json")
 
 ($ call-with-server "tests/json-server.scm"
-   (^p
-    (test* "json request/response" "{\"count\":0}"
-           (values-ref (http-get *server* "/") 2))
-    (test* "json request/response" "{\"count\":101}"
-           (values-ref (http-post *server* "/" "{\"count\":100}") 2))
-    (test* "json request/response (bad body)"
-           '("400" #/^invalid json/)
-           (receive (code hdrs body)
-               (http-post *server* "/" "[bad")
-             (list code body))
-           (^[a b] (and (equal? (car a) (car b))
-                        ((cadr a) (cadr b)))))))
+   (^[p s t]
+     (test* "json request/response" "{\"count\":0}"
+            (values-ref (http-get s "/") 2))
+     (test* "json request/response" "{\"count\":101}"
+            (values-ref (http-post s "/" "{\"count\":100}") 2))
+     (test* "json request/response (bad body)"
+            '("400" #/^invalid json/)
+            (receive (code hdrs body)
+                (http-post s "/" "[bad")
+              (list code body))
+            (^[a b] (and (equal? (car a) (car b))
+                         ((cadr a) (cadr b)))))))
 
 ;;;
 (test-section "let-params")
@@ -186,30 +186,30 @@
 (use rfc.cookie)
 
 ($ call-with-server "tests/let-params.scm"
-   (^p
-    (define (req path . args)
-      (read-from-string (values-ref (apply http-get *server* path args) 2)))
-    (parameterize ([http-user-agent "makiki-test"])
-      (test* "let-params 1"
-             '("moo" doo "zoo" 123456 "makiki-test" #f #f -1)
-             (req "/zoo/foo-123456?param1=moo&poo=doo"))
-      (test* "let-params 2"
-             '(none #t "zoo" 654321 "makiki-test" (a b c) "coo" 4934)
-             (req "/zoo/foo-654321"
-                  :x-header2 "(a b c)"
-                  :cookie "cookie1=coo;cookie2=4934"))
-      )))
+   (^[p s t]
+     (define (req path . args)
+       (read-from-string (values-ref (apply http-get s path args) 2)))
+     (parameterize ([http-user-agent "makiki-test"])
+       (test* "let-params 1"
+              '("moo" doo "zoo" 123456 "makiki-test" #f #f -1)
+              (req "/zoo/foo-123456?param1=moo&poo=doo"))
+       (test* "let-params 2"
+              '(none #t "zoo" 654321 "makiki-test" (a b c) "coo" 4934)
+              (req "/zoo/foo-654321"
+                   :x-header2 "(a b c)"
+                   :cookie "cookie1=coo;cookie2=4934"))
+       )))
 
 ;;;
 (test-section "server termination")
 
 (test* "/a" 1
        ($ call-with-server/exit-status "tests/termination.scm"
-          (^_ (http-get *server* "/a"))))
+          (^[p s t] (http-get s "/a"))))
 
 (test* "/b" 2
        ($ call-with-server/exit-status "tests/termination.scm"
-          (^_ (http-get *server* "/b"))))
+          (^[p s t] (http-get s "/b"))))
 
 ;;;
 (test-section "profiling")
@@ -222,14 +222,14 @@
              (begin
                (sys-unlink *profiler.out*)
                ($ call-with-server "tests/profiler.scm"
-                  (^_ (http-get *server* "/profile")))
+                  (^[p s t] (http-get s "/profile")))
                (and (file-exists? *profiler.out*)
                     (> (file-size *profiler.out*) 0))))
       (test* "profile output" #f
              (begin
                (sys-unlink *profiler.out*)
                ($ call-with-server "tests/profiler.scm"
-                  (^_ (http-get *server* "/noprofile")))
+                  (^[p s t] (http-get s "/noprofile")))
                (and (file-exists? *profiler.out*)
                     (> (file-size *profiler.out*) 0)))))
   (sys-unlink *profiler.out*))
