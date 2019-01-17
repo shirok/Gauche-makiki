f853dcf18d4ba9fdfa8e3bf9aefb31da4ff25df1
Shiro Kawai <shiro@acm.org>
2019-01-16 14:03:11 -1000

    Update configure for Gauche 0.9.7 as well

--- package.scm
+++ package.scm
@@ -2,6 +2,7 @@
   :version "0.2"
   :description "Simple multithreaded http server"
   :require (("Gauche" (>= "0.9.7")))
+  :providing-modules (makiki)
   :maintainers ("shiro@acm.org")
   :authors ("Shiro Kawai <shiro@acm.org>")
   :homepage "http://github.com/shirok/Gauche-makiki"
